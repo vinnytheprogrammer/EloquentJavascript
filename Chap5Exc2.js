a9023bc04bcdbd6751107c2964a66cd08714c32f
vinnytheprogrammer <58215501+vinnytheprogrammer@users.noreply.github.com>
2019-12-26 19:46:16 +0530

    Update Chap5Exc2.js

--- Chap5Exc2.js
+++ Chap5Exc2.js
@@ -11,4 +11,5 @@ loop(3, n => n > 0,  n => {n = n - 1; return n; }, console.log);
 // → 3
 // → 2
 // → 1
-// i=update(i) ( Why thsi assignment is required ? Answer ; SCOPE of the i inside update is different from this i ..but please consider the revision
+// i=update(i) ( Why this assignment is required ? Answer ; SCOPE of the i inside update is different from this i ..but please consider the revision
+// CONFIRMED: the assignment is required as the scopes are different 
