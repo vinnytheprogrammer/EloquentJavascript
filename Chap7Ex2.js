cf700c8073f63e2591d3f4a76278e769370f13bf
vinnytheprogrammer <58215501+vinnytheprogrammer@users.noreply.github.com>
2021-01-12 00:05:32 +0530

    Update Chap7Ex2.js

--- Chap7Ex2.js
+++ Chap7Ex2.js
@@ -1,36 +1,11 @@
-function yourRobot({place, parcels}, route) {
-  let parcelRoutes = [];
-  for(let p of parcels){
-    route = [];
-  	if (route.length == 0) {
-  	  if (p.place != place) {
-  	    route = findRoute(roadGraph, place, p.place);
-  	  } else {
-  	    route = findRoute(roadGraph, place, p.address);
-  	  }
-  	}
-  	parcelRoutes.push(route);
-  }
-
- 
-  for(let q=0; q< parcelRoutes.length-1; q++){
-        if(parcelRoutes[q].length === parcelRoutes[q+1].length){
-            if( parcelRoutes[q][0] !== place){
-          		route = parcelRoutes[q] ;
-            }
-            else {
-              route = parcelRoutes[q+1];
-            }  
-        }
-        else if(parcelRoutes[q].length < parcelRoutes[q+1].length){
-          route = parcelRoutes[q];
-        }
-        else {
-          route =  parcelRoutes[q+1];
-        }
-  
+function goalOrientedRobot({place, parcels}, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
   }
   return {direction: route[0], memory: route.slice(1)};
-  
 }
-runRobot(VillageState.random(), yourRobot, []);
